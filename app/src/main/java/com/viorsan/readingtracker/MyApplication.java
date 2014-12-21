03f525ba0632beed81aa726f5a2d96232f002b36
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2014-12-21 15:17:41 +0600

    make it possible to disable Parse's crash handling from test harness and/or activate regular Parse Platform with different AppId from test harness and not application's code

--- app/src/main/java/com/viorsan/readingtracker/MyApplication.java
+++ app/src/main/java/com/viorsan/readingtracker/MyApplication.java
@@ -1,5 +1,7 @@
 package com.viorsan.readingtracker;
 
+import android.util.Log;
+
 import com.parse.*;
 
 
@@ -11,18 +13,45 @@ import com.parse.*;
 
 
 public class MyApplication extends android.app.Application {
+    public static final String TAG = "MyApplication";
+    protected boolean useParseCrashReporting=true;//should we activate Parse's crash reporting ourselves?
+    protected boolean initParse=true;//should we init Parse ourselves?
+    protected boolean testHarnessActive=false;//we are running under Robolectric's test harness
     @Override
     public void onCreate() {
 
-
-        // Enable Parse-based Crash Reporting
-        ParseCrashReporting.enable(this);
-        //enable local datastore (we are write-mostly anyway)
-        //Parse.enableLocalDatastore(this);
-        //init Parse
-        Parse.initialize(this,BuildConfig.PARSE_APP_ID,BuildConfig.PARSE_CLIENT_KEY);
-        //enable automatic user support support
-        //ParseUser.enableAutomaticUser();
+        System.out.println(TAG +":OnCreate");
+        if (testHarnessActive) {
+            System.out.println(TAG +":Test harness active");
+            Log.d(TAG,"Test harness active");
+        }
+        else
+        {
+            System.out.println(TAG +":Test harness is not active");
+            Log.d(TAG,"Test harness is not active");
+        }
+        //Test harness could disallow us to do this
+        if (useParseCrashReporting) {
+            // Enable Parse-based Crash Reporting
+            Log.d(TAG,"Activating Parse's crash reporting");
+            ParseCrashReporting.enable(this);
+        }
+        else {
+            System.out.println(TAG+":Don't activating Parse's crash reporting. test harness said so");
+        }
+        //Test harness could disallow us to do this
+        if (initParse) {
+            Log.d(TAG,"Performing Parse's initialization");
+            //enable local datastore (we are write-mostly anyway)
+            //Parse.enableLocalDatastore(this);
+            //init Parse
+            Parse.initialize(this,BuildConfig.PARSE_APP_ID,BuildConfig.PARSE_CLIENT_KEY);
+            //enable automatic user support support
+            //ParseUser.enableAutomaticUser();
+        }
+        else {
+            System.out.println(TAG+":Don't performing Parse's initialization. test harness said so");
+        }
 
         //activate extra logging to debug login system
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
@@ -32,6 +61,7 @@ public class MyApplication extends android.app.Application {
         //this could cause issues if there is no cached user
         ParseACL.setDefaultACL(new ParseACL(), true);
 
+        System.out.println(TAG +":OnCreate, calling super's one");
         super.onCreate();
     }
 }
--- app/src/test/java/com/viorsan/readingtracker/TestMyApplication.java
+++ app/src/test/java/com/viorsan/readingtracker/TestMyApplication.java
@@ -3,6 +3,8 @@ package com.viorsan.readingtracker;
 import android.app.Application;
 import android.os.Build;
 
+import com.parse.Parse;
+
 import java.lang.reflect.Method;
 
 import org.junit.Before;
@@ -27,10 +29,21 @@ public class TestMyApplication extends MyApplication
 
     public static final String TAG = "TestMyApplication";
 
+    // init Parse Platform using our test keys
+    private void initParseForTests() {
+        Parse.initialize(this, BuildConfig.PARSE_APP_ID_FOR_TEST_HARNESS, BuildConfig.PARSE_CLIENT_KEY_FOR_TEST_HARNESS);
+    }
     @Override
     public void onCreate() {
         System.out.println(TAG +":OnCreate");
+        super.useParseCrashReporting=false;//don't use Parse's Crash Reporting (or any other)
+        super.initParse=false;//don't init Parse Platform in our app class (we do this ourselves)
+        super.testHarnessActive=true;//mark test harness as active (This is easy but not fully correct workaround Parse Analytics,etc issues)
+        initParseForTests();
+        System.out.println(TAG +":OnCreate. Done. Calling super.OnCreate()");
         super.onCreate();
+        System.out.println(TAG +":OnCreate. super.OnCreate() done");
+
     }
     @Override public void beforeTest(Method method) {
         System.out.println(TAG +":beforeTest:"+method.toString());
