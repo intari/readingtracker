b1b931060ada53167ae88061e07f95d3063d3daa
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2014-12-22 13:27:04 +0600

    modified my emulated ParseUser to work more like real ParseUser

--- app/src/main/java/com/viorsan/readingtracker/ParseUser_TestLoggedIn.java
+++ app/src/main/java/com/viorsan/readingtracker/ParseUser_TestLoggedIn.java
@@ -1,10 +1,22 @@
 package com.viorsan.readingtracker;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
 import com.parse.ParseClassName;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 22.12.14.
@@ -27,8 +39,8 @@ public class ParseUser_TestLoggedIn extends ParseUser {
         System.out.println(TAG +":constructor called");
         emulatedData=new HashMap<String, Object>();
         emulatedObjectId="ZATRIX";
-        emulatedEmail="brideOfTheSunGod@example.net";
-        emulatedUsername="BrideOfTheSun";
+        put("email","brideOfTheSunGod@example.net");
+        put("username","BrideOfTheSun");
         put("name","Bride of the Sun");
         System.out.println(TAG +":constructor finished");
     }
@@ -51,36 +63,30 @@ public class ParseUser_TestLoggedIn extends ParseUser {
         System.out.println(TAG +":fetchIfNeeded() called");
         return this;
     }
-    public String getEmail() {
-        return emulatedEmail;
+    public void setUsername(String username) {
+        System.out.println(TAG +":setUsername("+username+") called");
+        this.put("username", username);
     }
+
     public String getUsername() {
-        return emulatedUsername;
+        return this.getString("username");
     }
-    public void setEmail(String newEmail) {
-        System.out.println(TAG +":setEmail("+newEmail+") called");
-        emulatedEmail=newEmail;
+
+    public void setEmail(String email) {
+        System.out.println(TAG +":setEmail("+email+") called");
+        put("email", email);
     }
-    public void setUsername(String newUsername) {
-        System.out.println(TAG +":setUsername("+newUsername+") called");
-        emulatedUsername=newUsername;
+
+    public String getEmail() {
+        return getString("email");
     }
+
     public void setObjectId(String newObjectId) {
         System.out.println(TAG +":setObjectId("+newObjectId+") called");
         emulatedObjectId=newObjectId;
     }
     public String getObjectId() {
         return emulatedObjectId;
-    }
-    public void put(String key,Object value) {
-        if (value!=null) {
-            System.out.println(TAG +":put("+key+","+value.toString()+") called");
-        }
-        else
-        {
-            System.out.println(TAG +":put("+key+",null) called");
-        }
-        emulatedData.put(key,value);
     }
     public Object get(String key) {
         return emulatedData.get(key);
@@ -91,6 +97,37 @@ public class ParseUser_TestLoggedIn extends ParseUser {
     public void setACL(com.parse.ParseACL acl) {
         System.out.println(TAG +":setACL() called");
     }
+
+
+    public void remove(String key) {
+        if("username".equals(key)) {
+            throw new IllegalArgumentException("Can\'t remove the username key.");
+        } else {
+            emulatedData.remove(key);
+        }
+    }
+    static boolean isValidType(Object value) {
+        return value instanceof JSONObject || value instanceof JSONArray || value instanceof String || value instanceof Number || value instanceof Boolean || value == JSONObject.NULL || value instanceof ParseObject || value instanceof ParseACL || value instanceof ParseFile || value instanceof ParseGeoPoint || value instanceof Date || value instanceof byte[] || value instanceof List || value instanceof Map || value instanceof ParseRelation;
+    }
+
+    public void put(String key, Object value) {
+        if(key == null) {
+            throw new IllegalArgumentException("key may not be null.");
+        } else if(value == null) {
+            throw new IllegalArgumentException("value may not be null.");
+        } else if(!ParseUser_TestLoggedIn.isValidType(value)) {
+            throw new IllegalArgumentException("invalid type for value: " + value.getClass().toString());
+        } else {
+            System.out.println(TAG +":put("+key+","+value.toString()+") called");
+            emulatedData.put(key,value);
+
+        }
+    }
+
+    public boolean has(String key) {
+        return emulatedData.containsKey(key);
+    }
+
 
 }
 
