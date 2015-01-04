14cf95b4407cd1766ab34ad5a9a6d47fc4c02fd0
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2015-01-04 12:49:45 +0600

    correctly configure animations for tests

---
+++ app/src/test/java/com/viorsan/readingtracker/MyInstrumentationTestCase.java
@@ -0,0 +1,39 @@
+package com.viorsan.readingtracker;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+
+/**
+ * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 04.01.15.
+ */
+
+public class MyInstrumentationTestCase extends ActivityInstrumentationTestCase2<MyActivity> {
+
+    public static final String TAG = "ReadingTracker::MyInstrumentationTestCase";
+    private SystemAnimations systemAnimations;
+
+    public MyInstrumentationTestCase() {
+        super(MyActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        Log.d(TAG,"setUp() before super()");
+        super.setUp();
+        Log.d(TAG,"setUp() before disabling animations");
+        systemAnimations = new SystemAnimations(getInstrumentation().getContext());
+        systemAnimations.disableAll();
+        Log.d(TAG,"setUp() before getActivity");
+        getActivity();
+        Log.d(TAG,"setUp() after getActivity");
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        Log.d(TAG,"tearDown(), before super()");
+        super.tearDown();
+        systemAnimations.enableAll();
+        Log.d(TAG,"tearDown(), after enabling animations");
+    }
+}
---
+++ app/src/test/java/com/viorsan/readingtracker/SystemAnimations.java
@@ -0,0 +1,62 @@
+package com.viorsan.readingtracker;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by Dmitriy Kazimirov, e-mail:dmitriy.kazimirov@viorsan.com on 04.01.15.
+ * based off https://gist.github.com/xrigau/11284124
+ */
+class SystemAnimations {
+
+    private static final String ANIMATION_PERMISSION = "android.permission.SET_ANIMATION_SCALE";
+    private static final float DISABLED = 0.0f;
+    private static final float DEFAULT = 1.0f;
+    public static final String TAG = "SystemAnimations";
+
+    private final Context context;
+
+    SystemAnimations(Context context) {
+        this.context = context;
+    }
+
+    void disableAll() {
+        int permStatus = context.checkCallingOrSelfPermission(ANIMATION_PERMISSION);
+        if (permStatus == PackageManager.PERMISSION_GRANTED) {
+            setSystemAnimationsScale(DISABLED);
+        }
+    }
+
+    void enableAll() {
+        int permStatus = context.checkCallingOrSelfPermission(ANIMATION_PERMISSION);
+        if (permStatus == PackageManager.PERMISSION_GRANTED) {
+            setSystemAnimationsScale(DEFAULT);
+        }
+    }
+
+    private void setSystemAnimationsScale(float animationScale) {
+        try {
+            Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
+            Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
+            Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
+            Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
+            Method setAnimationScales = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
+            Method getAnimationScales = windowManagerClazz.getDeclaredMethod("getAnimationScales");
+
+            IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
+            Object windowManagerObj = asInterface.invoke(null, windowManagerBinder);
+            float[] currentScales = (float[]) getAnimationScales.invoke(windowManagerObj);
+            for (int i = 0; i < currentScales.length; i++) {
+                currentScales[i] = animationScale;
+            }
+            setAnimationScales.invoke(windowManagerObj, new Object[]{currentScales});
+        } catch (Exception e) {
+            Log.e(TAG, "Could not change animation scale to " + animationScale + " :'(");
+        }
+    }
+}
