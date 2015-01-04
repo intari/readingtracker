0f22aa97c30fbe789b1b7c55954de02b00da238b
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2015-01-04 16:39:23 +0600

    don't daemonize

--- runEmulator.sh
+++ runEmulator.sh
@@ -5,9 +5,9 @@ export ANDROID_ABI=armeabi-v7a
 echo "Creating emulator..."
 echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
 echo "Starting emulator..."
-emulator -avd test -no-skin -no-audio -no-window &
+emulator -avd test -no-skin -no-audio -no-window 
 echo "Emulator started. Waiting for startup to finish"
 adb wait-for-device
 echo "Pressing menu in emulator"
-adb shell input keyevent 82 &
+adb shell input keyevent 82 
 echo "Android emulator ready"
