f3cbd0deac88eccd2e355f35bed6c1642b6b93ac
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2015-01-05 09:51:59 +0600

    remove all old logcat logs

--- logcatCollect.sh
+++ logcatCollect.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+rm -rf logcat_*.log
 #sleep 5
 devices=`adb devices | grep 'device$' | cut -f1`
 pids=""
@@ -8,7 +9,6 @@ for device in $devices
 do
     log_file="logcat_$device-`date +%d-%m-%H_%M_%S`.log"
     echo "Logging device $device to \"$log_file\""
-    rm -rf $logfile
     adb -s $device logcat -v time -d > $log_file
     pids="$pids $!"
 done
