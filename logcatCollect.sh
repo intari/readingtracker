60b6873f0269873226033858fff7203b2b9786fa
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2015-01-05 09:41:14 +0600

    change logcat file naming to remove : from them

--- logcatCollect.sh
+++ logcatCollect.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-sleep 5
+#sleep 5
 devices=`adb devices | grep 'device$' | cut -f1`
 pids=""
 
 for device in $devices
 do
-    log_file="logcat_$device-`date +%d-%m-%H:%M:%S`.log"
+    log_file="logcat_$device-`date +%d-%m-%H_%M_%S`.log"
     echo "Logging device $device to \"$log_file\""
     rm -rf $logfile
     adb -s $device logcat -v time -d > $log_file
