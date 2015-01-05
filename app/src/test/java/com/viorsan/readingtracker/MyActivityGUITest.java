9988aadb740bc29ca1ee14b9c66684c9f4abf740
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2015-01-05 20:36:14 +0600

    wait a little after login is completed and test that parse is in correct state

--- app/src/test/java/com/viorsan/readingtracker/MyActivityGUITest.java
+++ app/src/test/java/com/viorsan/readingtracker/MyActivityGUITest.java
@@ -1,5 +1,6 @@
 package com.viorsan.readingtracker;
 
+import com.parse.ParseUser;
 import com.viorsan.readingtracker.MyActivity;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -7,10 +8,12 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static org.hamcrest.CoreMatchers.*;
 
 import android.test.ActivityInstrumentationTestCase2;
 
@@ -30,6 +33,7 @@ import com.viorsan.readingtracker.MyActivity;
 public class MyActivityGUITest extends MyInstrumentationTestCase { // ActivityInstrumentationTestCase2<MyActivity> {
 
     public static final String TAG = "ReadingTrackerTests::MyActivityGUITest";
+    public static final int DEFAULT_SLEEP_TIME = 1337;//sometimes even 100 ms ok
 
     public MyActivityGUITest() {
         super();//MyActivity.class);
@@ -99,18 +103,28 @@ public class MyActivityGUITest extends MyInstrumentationTestCase { // ActivityIn
                         closeSoftKeyboard());
 
         Log.d(TAG,"Login button pressed now checking");
-        Thread.sleep(100,0);//Highscreen Boost IIse, or Android 4.3, or my stupidity but without this test will fail
+        Thread.sleep(DEFAULT_SLEEP_TIME,0);//Highscreen Boost IIse, or Android 4.3, or my stupidity but without this test will fail
         Log.d(TAG,"Done sleeping. checking  button pressed now checking");
 
         onView(withId(R.id.parse_login_button))
                 .perform(click());
         //we should now be on initial screen
         //but in logged in state
+        Log.d(TAG,"Wait a little until we complete login");
+        //TODO:'idling resources!'
+        Thread.sleep(DEFAULT_SLEEP_TIME,0);
+        Log.d(TAG,"Should now be logged in");
+        ParseUser currentUser=ParsePlatformUtils.getCurrentParseUser();
+        assertThat("Parse's currentUser should not be null after login",currentUser,is(not(null)));
 
+        Log.d(TAG, "Validating we were logged in correctly");
         onView(withId(R.id.login_or_logout_button))
             .check(matches(withText(R.string.profile_logout_button_label)));
 
-        Log.d(TAG,"Validating we were logged in correctly");
+        Log.d(TAG,"Logging out");
+        onView(withId(R.id.login_or_logout_button))
+                .perform(click());
+
     }
 
 }
