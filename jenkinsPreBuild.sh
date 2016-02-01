2070187d08afca686dfdd5a54850908099389eec
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2016-02-01 14:08:29 +0600

    kill old android emulator

--- jenkinsPreBuild.sh
+++ jenkinsPreBuild.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Performing initial tasks..."
-#export $ANDROID_TARGET="Google Inc.:Google APIs:22"
-#export $ANDROID_ABI=google_apis/armeabi-v7a
+
+echo "Stopping old instances of android emulator"
+killall emulator64-arm
 
 echo "Creating Android Virtual Device..."
 android create avd --force -n myemulator-google -t "Google Inc.:Google APIs:22" --abi google_apis/armeabi-v7a
