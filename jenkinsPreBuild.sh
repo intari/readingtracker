78234e253c44118acb43e94b634583f7bb582fb9
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2016-02-01 14:52:58 +0600

    more timeout hacking

--- jenkinsPreBuild.sh
+++ jenkinsPreBuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Performing initial tasks..."
 export ADB_INSTALL_TIMEOUT=8 # minutes (2 minutes by default)
-
+adb start-server
 echo "Stopping old instances of android emulator"
 killall emulator64-arm
 killall emulator-arm
@@ -9,10 +9,11 @@ echo "Creating Android Virtual Device..."
 android create avd --force -n myemulator -t "Google Inc.:Google APIs:22" --abi google_apis/armeabi-v7a
 echo "Starting emulator..."
 emulator64-arm -avd myemulator -no-audio -no-window &
+sleep 30
 echo "Wait until emulator starts up"
 ./android-wait-for-emulator.sh
 #unlock emulator screen
-sleep 30
+sleep 60
 echo "Pressing menu in emulator"
 adb shell input keyevent 82
 
