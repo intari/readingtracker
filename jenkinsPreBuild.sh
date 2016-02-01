20af7e74fdff9b14c365d69ba68f8d1cef78b2e9
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2016-02-01 14:49:29 +0600

    increase timeouts

--- jenkinsPreBuild.sh
+++ jenkinsPreBuild.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Performing initial tasks..."
+export ADB_INSTALL_TIMEOUT=8 # minutes (2 minutes by default)
 
 echo "Stopping old instances of android emulator"
 killall emulator64-arm
-
+killall emulator-arm
 echo "Creating Android Virtual Device..."
-android create avd --force -n myemulator-google -t "Google Inc.:Google APIs:22" --abi google_apis/armeabi-v7a
-
+android create avd --force -n myemulator -t "Google Inc.:Google APIs:22" --abi google_apis/armeabi-v7a
 echo "Starting emulator..."
-emulator64-arm -avd myemulator-google -no-audio -no-window &
+emulator64-arm -avd myemulator -no-audio -no-window &
 echo "Wait until emulator starts up"
 ./android-wait-for-emulator.sh
 #unlock emulator screen
